7fb5196a6ea45261c747f7d545872139a037de38
Wei Lian <weilian2003@live.com>
2024-05-23 00:54:45 +0800

    Add authorisation check for edit page

--- src/app/(userFacing)/products/[id]/edit/page.tsx
+++ src/app/(userFacing)/products/[id]/edit/page.tsx
@@ -1,9 +1,16 @@
 import { getProductById } from "@/app/_actions/product";
 import ProductForm from "@/components/ProductForm";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function EditPage({ params }: { params: { id: string } }) {
   const product = await getProductById(parseInt(params.id));
-  if (!product) return;
+  if (!product) redirect("/"); // no such product
+  
+  const { getUser, isAuthenticated }= getKindeServerSession();
+  if (!await isAuthenticated()) redirect("/api/auth/login");
+  const kindeUser = await getUser();
+  if (product.sellerKindeId != kindeUser?.id) redirect("/"); // unauthorise, can throw in future
   return (
     <ProductForm product={product}/>
   )
