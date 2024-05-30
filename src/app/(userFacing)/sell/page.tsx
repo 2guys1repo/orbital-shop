15e4d461d2ee0522559501ee300d0b2d5d9c5c0b
Wei Lian <weilian2003@live.com>
2024-05-30 17:12:17 +0800

    Init uploadthing

--- src/app/(userFacing)/sell/page.tsx
+++ src/app/(userFacing)/sell/page.tsx
@@ -1,10 +1,11 @@
 import { getAuthenticatedUser } from "@/app/_actions/auth";
 import ProductForm from "@/components/ProductForm";
+import Utform from "./Utform";
 
 // Page for users to create new product
 export default async function SellPage() {
   await getAuthenticatedUser();
   return (
-    <ProductForm />
+    <Utform />
   )
 }
