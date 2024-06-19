53928115bae2f770b63e958f6eee3f0112cf6978
Wei Lian <weilian2003@live.com>
2024-06-19 16:56:03 +0800

    Add search and filter functionality

--- src/app/(userFacing)/page.tsx
+++ src/app/(userFacing)/page.tsx
@@ -1,7 +1,5 @@
 import ProductCard from "@/components/ProductCard"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
-import { Card } from "@/components/ui/card"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import { getAllProducts } from "../_actions/product"
 import { getUserNamesByIds } from "../_actions/user"
@@ -9,7 +7,7 @@ import { getUserNamesByIds } from "../_actions/user"
 // Main home page for users
 export default function HomePage() {
   return (
-    <>
+    <div className="">
       {/* Header banner section */}
       <header className="bg-red-50 rounded-md h-60 relative -mt-1">
         <div className="flex flex-col p-6 h-full justify-center gap-6 ">
@@ -26,21 +24,22 @@ export default function HomePage() {
         </div>
       </header>
       <main>
-        <div className="w-full">
+        <div className="w-full min-h-[85vh]">
           <h2 className="mt-4 mb-2 text-2xl font-semibold">Featured Products</h2>
           <ProductCarousel />
         </div>
       </main>
-    </>
+    </div>
   )
 }
 
 async function ProductCarousel() {
-  let temp = await getAllProducts();
-  temp = [...temp, ...temp]
-  const products = temp  // TODO can get by likes/reviews
+  // let temp = await getAllProducts();
+  // temp = [...temp, ...temp]
+  // const products = temp // mock data
+  const products = await getAllProducts() // TODO can get by likes/reviews
   const sellerIds = products.map(product => product.sellerId)
-  const sellerNames = await getUserNamesByIds(sellerIds)
+  const sellerNameDict = await getUserNamesByIds(sellerIds) // key is user id, val is name
   return (
     <Carousel
       className="w-full"
@@ -51,22 +50,9 @@ async function ProductCarousel() {
     >
       <CarouselContent className="w-full -ml-1">
         {products.map(product => (
-          <CarouselItem key={product.id} className="basis-1/5 pl-1">
-            <Card className="flex flex-col px-4 ">
-              {/* Top row with seller info */}
-              <div className="flex gap-2 items-center mt-1 mb-2">
-                <Avatar>
-                  <AvatarImage src="" />
-                  <AvatarFallback>{sellerNames[product.sellerId][0]}</AvatarFallback>
-                </Avatar>
-                <div className="flex flex-col">
-                  <p className="text-sm font-semibold">{sellerNames[product.sellerId]}</p>
-                  <p className="text-sm text-muted-foreground">{Math.floor(Math.random() * 10) + 2} days ago</p>
-                </div>
-              </div>
-              {/* Product info */}
-              <ProductCard product={product} width={220} />
-            </Card>
+          <CarouselItem key={product.id} className="basis-1/2 md:basis-1/3 lg:basis-1/5 pl-1 ">
+            {/* TODO add seller img path */}
+            <ProductCard key={product.id} product={product} sellerName={sellerNameDict[product.sellerId]} sellerImgPath="" />
           </CarouselItem>
         ))}
       </CarouselContent>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,37 @@
+"use client"
+
 import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
+// main search bar on the nav
 export default function SearchBar() {
+  const [query, setQuery] = useState("")
+  const router = useRouter()
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const params = new URLSearchParams({
+      query: query
+    })
+    // directs the user to Search page to view their queried results
+    if (query === "") params.delete("query")
+    router.push(`/search/?${params}`)
+  }
   return (
-    <div className="relative">
+    <form
+      className="relative w-1/2 mr-auto flex gap-4 items-center min-w-fit"
+      onSubmit={e => handleSubmit(e)}
+    >
       <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-      <Input placeholder="Search..." className="pl-8 focus-visible:ring-gray-500" />
-    </div>
+      <Input
+        className="pl-8 focus-visible:ring-transparent rounded-xl border-gray-800 border-2"
+        placeholder="Search for anything..."
+        onChange={e => setQuery(e.target.value)}
+        value={query}
+      />
+      <Button size="sm" type="submit" className="hidden sm:block">Search</Button>
+    </form>
   )
-}
+};
