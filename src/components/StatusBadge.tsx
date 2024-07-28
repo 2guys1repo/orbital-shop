ee6e911f9bc3f902435e3ec1fbeee3dea52d64e2
Marc Lim <marchjlim@gmail.com>
2024-07-28 20:24:13 +0800

    fixed bug

--- src/components/StatusBadge.tsx
+++ src/components/StatusBadge.tsx
@@ -1,40 +1,50 @@
 import { capitalizeFirstLetter } from "@/lib/utils";
 import { Badge } from "./ui/badge";
 import { OrderStatus, ReportStatus } from "@prisma/client";
-import { report } from "process";
 
-// Returns a colored badge based on status
 export default function StatusBadge({ orderStatus, reportStatus }: { orderStatus?: OrderStatus, reportStatus?: ReportStatus }) {
   let color = "";
+  let statusText = "";
+
   if (orderStatus) {
     switch (orderStatus) {
       case OrderStatus.PENDING:
         color = "cursor-pointer bg-yellow-500 text-white";
+        statusText = "Pending";
         break;
       case OrderStatus.SHIPPED:
         color = "cursor-pointer bg-green-500 text-white";
+        statusText = "Shipped";
         break;
       case OrderStatus.REJECTED:
         color = "cursor-pointer bg-red-500 text-white";
+        statusText = "Rejected";
+        break;
+      default:
+        color = "cursor-pointer bg-gray-500 text-white";
+        statusText = "Unknown";
         break;
     }
   } else if (reportStatus) {
     switch (reportStatus) {
       case ReportStatus.PENDING:
         color = "cursor-pointer bg-yellow-500 text-white";
+        statusText = "Pending";
         break;
       case ReportStatus.ACCEPTED:
         color = "cursor-pointer bg-green-500 text-white";
+        statusText = "Accepted";
         break;
       case ReportStatus.REJECTED:
         color = "cursor-pointer bg-red-500 text-white";
+        statusText = "Rejected";
+        break;
+      default:
+        color = "cursor-pointer bg-gray-500 text-white";
+        statusText = "Unknown";
         break;
     }
   }
-  
-  return <Badge className={color}>
-    {reportStatus ? capitalizeFirstLetter(reportStatus)
-                  : capitalizeFirstLetter(orderStatus)}
-  </Badge>
-}
 
+  return <Badge className={color}>{capitalizeFirstLetter(statusText)}</Badge>;
+}
